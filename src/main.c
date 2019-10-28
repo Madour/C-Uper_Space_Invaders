98fd5b4f8314c6225af01a9ebde46339ed6df3a6
Modar Nasser <modar1999@gmail.com>
2019-10-28 22:36:52 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -562,7 +562,7 @@ int main(int argc, char const *argv[])
 
 				case 4:
 					game_over_pos.x=150;
-					game_over.y=-100;
+					game_over_pos.y=-100;
 					level_index+=1;
 					load_level(&current_level, level_index, alien_list, shields_list, &boss);
 					show_wave_nb = 1;
